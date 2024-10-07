b76ef17d1f0f92af7be8bcf033e84e651d3087c0
antetigmo <teixopoison2@gmail.com>
2024-10-06 23:37:27 -0300

    Generation relative to player
    Generate platforms relative to the player position(Temporary).
    
    Todo: Sse the lastPlataformPos instead of the playerPos.

--- scripts/platformGenerator.gd
+++ scripts/platformGenerator.gd
@@ -1,27 +1,45 @@
 extends Node2D
 
+@onready var game = get_parent()
 @onready var timer = Timer.new()
+
+#OBS:instead of playerPosX(temporary) use last platformPosX, this is temporary.
+
+var y = -140 # Height which will be spawned
+var gap = 80 # Jump gap
+var playerPosX
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(timer)
-	timer.wait_time = 2.0
+	timer.wait_time = 2
 	timer.start()
 
 	timer.connect("timeout", Callable(self, "generatePlatforms"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	playerPosX = int(game.get_node("Player").position.x - 1)
 
+func generatePlatforms(): # Generate platforms relative to the player position.
+	var x
+	
+	if playerPosX >= 120:
+		x = playerPosX - gap
+		createPlatform(x,y)	
+		return
+	elif playerPosX <= -120:
+		x = playerPosX + gap
+		createPlatform(x,y)	
+		return
 
-func generatePlatforms():
-	var x = randi() % 160 - 80
-	var y = -140
-
-	createPlatform(x,y)
-
+	var plusOrMinus = gap if randi() % 2 == 0 else -gap
+	print(plusOrMinus)
+	x = playerPosX +(plusOrMinus)
+	createPlatform(x,y)	
+		
+		
 func createPlatform(x,y): 
-	var game = get_parent()
 	var scene = preload("res://scenes/plataform.tscn")
 	var obj = scene.instantiate()	
 
